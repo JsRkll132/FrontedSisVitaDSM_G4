b844531f98d1fc292e0d88ccbe1e05bf8f57e9b4
RenzoDc-1 <renzo.delacruz1@unmsm.edu.pe>
2024-06-21 16:28:13 -0500

    Modelos actualizados

--- SisVita/app/src/main/java/com/example/sisvita/data/models/FormularioCompletadoModelResponse.kt
+++ SisVita/app/src/main/java/com/example/sisvita/data/models/FormularioCompletadoModelResponse.kt
@@ -7,8 +7,10 @@ data class FormularioCompletadoModelResponse(
     val apellido_paterno: String,
     val completado_formulario_id: Int,
     val nombres: String,
-    val suma_puntuacion: Any,
+    val suma_puntuacion: Int,
     val tipo_formulario: String,
     val fecha_completado :String,
-    val usuario_id: Int
+    val usuario_id: Int,
+    val paciente_id: Int,
+    val formulario_id : Int
 )
