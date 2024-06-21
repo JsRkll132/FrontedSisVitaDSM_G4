50d7d9755eb4458c7b15311973e6bcb9c7e3327f
marcoadjl <marco.dejesus@unmsm.edu.pe>
2024-06-21 01:35:45 -0500

    Commit_RespFormCompletPacient

---
+++ SisVita/app/src/main/java/com/example/sisvita/data/models/RespFormCompletPacient.kt
@@ -0,0 +1,8 @@
+package com.example.sisvita.data.models
+
+data class RespFormCompletPacient(
+    val pregunta: String,
+    val puntuacion: Int,
+    val respuesta: String,
+    val respuesta_id: Int
+)
